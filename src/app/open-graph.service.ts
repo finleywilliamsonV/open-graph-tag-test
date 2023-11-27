97b50f6da8a95e1eca9b799d7ee3b52d7feae42d
Finley Williamson V <finley.williamson@gmail.com>
2023-11-27 12:37:40 -0500

    Update OpenGraphService to add location-specific tags

--- src/app/open-graph.service.ts
+++ src/app/open-graph.service.ts
@@ -12,32 +12,54 @@ export class OpenGraphService {
   addMetaTags(location: Location) {
     // default tags
     this.meta.updateTag({ name: 'og:site_name', content: 'Site Name' });
-    this.meta.updateTag({ name: 'og:url', content: '<url goes here>' });
+    this.meta.updateTag({ name: 'og:url', content: window.location.href });
     this.meta.updateTag({ name: 'og:type', content: 'website' });
     this.meta.updateTag({
       name: 'og:description',
       content: 'Testing Open Graph tags',
     });
-    this.meta.updateTag({
-      name: 'og:image',
-      content: `${window.location.origin}/assets/${location}.png`,
-    });
 
     // location-specific tags
+
+    // Home Page
     if (location === 'home') {
+      // title
       this.meta.updateTag({
         name: 'og:title',
         content: 'Home Page',
       });
-    } else if (location === 'test-one') {
+      // image
+      this.meta.updateTag({
+        name: 'og:image',
+        content: `${window.location.origin}/assets/home.png`,
+      });
+    }
+
+    // Test One Page
+    else if (location === 'test-one') {
+      // title
       this.meta.updateTag({
         name: 'og:title',
         content: 'Test One Page',
       });
-    } else if (location === 'test-two') {
+      // image
+      this.meta.updateTag({
+        name: 'og:image',
+        content: `${window.location.origin}/assets/${location}.png`,
+      });
+    }
+
+    // Test Two Page
+    else if (location === 'test-two') {
+      // title
       this.meta.updateTag({
         name: 'og:title',
         content: 'Test Two Page',
+      });
+      // image
+      this.meta.updateTag({
+        name: 'og:image',
+        content: `${window.location.origin}/assets/two.png`,
       });
     }
   }
